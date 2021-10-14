1b0de6b1e85c26cb35a47722188d54203c4c0178
Jarle Friestad <jfri@equinor.com>
2021-10-14 22:39:31 +0200

    Small change

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,14 +1,21 @@
-import { ThemeProvider } from '@/providers';
 import { ApolloProvider } from '@apollo/client';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import client from './apollo';
 import App from './App';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <ThemeProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
         <App />
       </ThemeProvider>
     </ApolloProvider>
